a89b9fc54ca706da6ff6fdb141aca6541963ae6b
Anderson de Oliveira Venturini <45111567+andersonoliveiraventurini@users.noreply.github.com>
2019-11-08 14:34:11 -0300

    ajustes layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+   shrink-to-fit=no">
+    <!-- Scripts -->
+   
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Bootstrap CSS - public/css/app.css-->
+  
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- caso não recebe nenhum valor o título da página será "Projeto 5.8"-->
+  
+    <title>@yield('title','Projeto 6')</title>
+    </head>
+    <body>
+        <div class="container">
+            @include('menu')
+            
+            @yield('content')
+        </div>
+    </body>
+   </html>
